7214c18a817aa24063b83ccca0f0d4d809c822a5
saikirandas076-dot <saikirandas076@gmail.com>
2026-06-23 21:47:31 +0530

    app.py

--- app.py
+++ app.py
@@ -78,12 +78,11 @@ uploaded_file = st.file_uploader(
 if uploaded_file:
 
     image = Image.open(uploaded_file).convert("RGB")
-
-    st.image(
-        image,
-        caption="Uploaded Image",
-        use_container_width=True
-    )
+ st.image(
+    image,
+    caption="Uploaded Image",
+    use_column_width=True
+)
 
     img = image.resize((224,224))
 
